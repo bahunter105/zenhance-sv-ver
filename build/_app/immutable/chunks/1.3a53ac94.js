e23c992c144b8e7879b9a408e99c8f82de8b88c9
Hunter Bourgeois <79214781+bahunter105@users.noreply.github.com>
2024-07-19 00:32:31 +0000

    updated root for spa

--- build/_app/immutable/chunks/1.3a53ac94.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../entry/error.svelte.73eb5c7e.js";export{t as component};
--- svelte.config.js
+++ svelte.config.js
@@ -8,7 +8,7 @@ const config = {
 	kit: {
 		adapter: adapter(),
 		paths: {
-			base: process.env.NODE_ENV === 'production' ? '/zenhance-sv-ver' : ''
+			base: process.env.NODE_ENV === 'production' ? '/zenhance-spa' : ''
 		}
 	}
 };
